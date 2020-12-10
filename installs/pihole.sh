0d936f3438f762c4a2943f8d3bc2c522a7b9d809
Silvio Schwarz <admin@silvioschwarz.com>
2020-12-10 01:15:43 +0100

    corrected configs

--- installs/pihole.sh
+++ installs/pihole.sh
@@ -6,10 +6,10 @@ sudo apt install unbound
 
 wget -O root.hints https://www.internic.net/domain/named.root
 sudo cp root.hints /var/lib/unbound/
-sudo cp pi-hole.conf /etc/unbound/unbound.conf.d/pi-hole.conf
+sudo cp ../configs/pi-hole.conf /etc/unbound/unbound.conf.d/pi-hole.conf
 
 sudo service unbound start
-dig pi-hole.net @127.0.0.1 -p 5353
+dig pi-hole.net @127.0.0.1 -p 5335
 
 #sudo ./firewallInstall.sh
 
