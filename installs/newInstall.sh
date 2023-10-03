edf06dec10fb40b8bc995d67089b205a41240119
silvioschwarz <admin@silvioschwarz.xyz>
2023-10-03 15:34:18 +0200

    updated setStaticIP.sh

--- installs/newInstall.sh
+++ installs/newInstall.sh
@@ -77,9 +77,10 @@ sudo systemctl enable vncserver-x11-serviced.service
 sudo apt-get install -y mlocate build-essential git
 sudo apt-get install -y libatlas-base-dev libssl-dev libffi-dev libxml2-dev libxslt-dev liblapack-dev libsuitesparse-dev libjpeg-dev zlib1g-dev libglib2.0-dev libgirepository1.0-dev libcairo2-dev libjpeg-dev libgif-dev libsdl1.2-dev libhdf5-dev
 
+sudo apt install python-dev
 
 git config --global user.name "Silvio Schwarz"
-git config --global user.email admin@silvioschwarz.com
+git config --global user.email admin@silvioschwarz.xyz
 
 ./updates/update.sh
 sudo raspi-config
