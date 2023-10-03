0033536a45be70c6e25782669b25796deb2209ce
Silvio Schwarz <admin@silvioschwarz.xyz>
2023-10-03 15:35:38 +0200

    updated setStaticIP.sh

--- installs/newInstall.sh
+++ installs/newInstall.sh
@@ -68,18 +68,10 @@ if [ ! $(cat /etc/timezone | egrep "^Etc/UTC" >/dev/null) ]; then
   echo "Done"
 fi
 
-sudo apt install realvnc-vnc-server realvnc-vnc-viewer
-
-
-sudo systemctl enable vncserver-virtuald.service
-sudo systemctl enable vncserver-x11-serviced.service
-
-sudo apt-get install -y mlocate build-essential git
-sudo apt-get install -y libatlas-base-dev libssl-dev libffi-dev libxml2-dev libxslt-dev liblapack-dev libsuitesparse-dev libjpeg-dev zlib1g-dev libglib2.0-dev libgirepository1.0-dev libcairo2-dev libjpeg-dev libgif-dev libsdl1.2-dev libhdf5-dev
-
-
 git config --global user.name "Silvio Schwarz"
-git config --global user.email admin@silvioschwarz.com
+git config --global user.email admin@silvioschwarz.xyz
 
-./updates/update.sh
+sudo apt update
+sudo apt upgrade -y
+
 sudo raspi-config
--- installs/setStaticIP.sh
+++ installs/setStaticIP.sh
@@ -1,26 +1,43 @@
-#! /bin/sh
+#! /bin/bash
 
 
 CURRENTIP=$(hostname -I | awk '{print $1}')
 ROUTERIP=$(ip r | grep "default via" | awk '{print $3}')
+DNSIP=$(grep "namesever" /etc/resolv.conf | awk '{print $2}')
+
+echo $DNS
+read 
 
 
-DNSIP=$(grep "namesever" /etc/resolv.conf)
+cat /etc/resolv.conf
+sudo cp /etc/resolv.conf /etc/resolv.conf.old
+
 
 read -p "Enter interface: " INTERFACE
 read -p "Enter static IP: " STATICIP
 
-echo $CURRENTIP
-echo $ROUTERIP
-echo $DNSIP
-echo $INTERFACE
-echo $STATICIP
+echo "current IP: " $CURRENTIP
+echo "Gateway: " $ROUTERIP
+echo "DNS IP: " $DNSIP
+echo "interface : "$INTERFACE
+echo "static IP: "$STATICIP
 
-#FILE="/etc/dhcpcd.conf"
-#LINE="interface eth0"
+read -p "Press any key to continue... " -n1 -s -t 5
 
-#grep $LINE $FILE || echo $LINE >> $FILE
+echo "write config to file  ..."
 
-#static_routers=[ROUTER IP]
-#static domain_name_servers=[DNS IP]
-#static ip_address=[STATIC IP ADDRESS YOU WANT]/24
+FILE="/etc/dhcpcd.conf"
+LINE="interface eth0"
+grep -qxF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+
+LINE="static routers=${ROUTERIP}"
+grep -qxF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+
+LINE="static domain_name_servers=${DNSIP}"
+grep -qxF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+
+LINE="static ip_address=${STATICIP}/24"
+grep -qxF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
